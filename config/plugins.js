9dccd5bd28bbe60502eb6c98e377afa40aa46b12
caidavies <caidavies37@gmail.com>
2024-02-28 20:46:04 +0100

    Gitignore

---
+++ config/plugins.js
@@ -0,0 +1,40 @@
+module.exports = ({ env }) => ({
+  'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET'),
+    },
+  },
+  'strapi-blurhash-plugin': {
+    enabled: true,
+    config: {
+        regenerateOnUpdate: true
+    }
+  },
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        baseUrl: 'https://throstles.s3.eu-west-2.amazonaws.com', // Or your CDN URL if different
+        rootPath: '', // Set this if you're using a specific path inside your bucket as the root for uploads
+        s3Options: {
+          
+          accessKeyId: 'AKIASCFUFUS4TDTQBBEV',
+          accessKeyId: env('AWS_ACCESS_KEY_ID'),
+          secretAccessKey: env('AWS_SECRET_KEY'),
+          region: 'eu-west-2', // Extracted from your S3 bucket URL
+          params: {
+            ACL: env('AWS_ACL', 'public-read'), // Default to public-read if not specified
+            signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60), // Default to 15 minutes if not specified
+            Bucket: 'throstles', // Extracted from your S3 bucket URL
+          },
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+  
+});
