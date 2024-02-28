00d82b75f218af0eedfb18dcf349fb776f0460c3
caidavies <caidavies37@gmail.com>
2024-02-28 20:26:38 +0100

    Updates

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,25 +1,37 @@
-module.exports = [
-  'strapi::errors',
+module.exports = ({ env }) => [
   {
     name: 'strapi::security',
     config: {
       contentSecurityPolicy: {
-        useDefaults: true,
         directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          upgradeInsecureRequests: null,
+          'script-src': ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'throstles.s3.eu-west-2.amazonaws.com',
+          ],
         },
-      },
+      }
     },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: '*',
+      credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+      headers: ['Content-Type', 'Authorization', 'X-Frame-Options', 'Origin', 'X-Requested-With']
+    }
   },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
   'strapi::body',
-  'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  'strapi::session',
+  'strapi::errors',
 ];
--- config/plugins.js
+++ config/plugins.js
@@ -4,18 +4,35 @@ module.exports = ({ env }) => ({
       jwtSecret: env('JWT_SECRET'),
     },
   },
+  'strapi-blurhash-plugin': {
+    enabled: true,
+    config: {
+        regenerateOnUpdate: true
+    }
+  },
   upload: {
     config: {
-      provider: 'cloudinary',
+      provider: 'aws-s3',
       providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
+        baseUrl: 'https://throstles.s3.eu-west-2.amazonaws.com', // Or your CDN URL if different
+        rootPath: '', // Set this if you're using a specific path inside your bucket as the root for uploads
+        s3Options: {
+          accessKeyId: 'AKIASCFUFUS4TDTQBBEV',
+          secretAccessKey: 'OyFp9WDm4uvPziAYJu3vANJM3+88N7kKspDPlBlw',
+          region: 'eu-west-2', // Extracted from your S3 bucket URL
+          params: {
+            ACL: env('AWS_ACL', 'public-read'), // Default to public-read if not specified
+            signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60), // Default to 15 minutes if not specified
+            Bucket: 'throstles', // Extracted from your S3 bucket URL
+          },
+        },
       },
       actionOptions: {
         upload: {},
+        uploadStream: {},
         delete: {},
       },
     },
   },
+  
 });
